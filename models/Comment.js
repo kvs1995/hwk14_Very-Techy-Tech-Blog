f97619f8585a7ef7845a83e98cce932b7486d67a
Yelena Belova <42819374+kvs1995@users.noreply.github.com>
2022-01-11 03:01:21 -0600

    Comment.js model build

--- models/Comment.js
+++ models/Comment.js
@@ -3,11 +3,49 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 //extend model 
+class Comment extends Model {}
 
 //Comment.init
+Comment.init(
 //id, content, date_created, post_id, user_id
-
-
-
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey:true, 
+            autoIncrement: true,
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false, 
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model:'post',
+                key:'id',
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model:'user',
+                key:'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false, 
+        freezeTableName: true,
+        underscored: true, 
+        modelName: 'comment',
+    }
+);
 
 //module export
+module.exports = Comment;
