2140fb16f1abfe10cd2c4f21422925379c5f5e51
Yelena Belova <42819374+kvs1995@users.noreply.github.com>
2022-01-11 03:18:30 -0600

    models/index.js model relationships build

--- models/index.js
+++ models/index.js
@@ -6,18 +6,41 @@ const Comment = require('./Comment');
 
 //User - Post
 //User has Many Posts
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
 
 //Post - User
 //Post belongs to User
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 
 //Post - Comment
 //Post has Many Comments
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
 
 //Comment - Post
 //Comment belongs Post
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
 
 //User - Comment
 //User has Many Comments
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
 
 //Comment - User
 //Comment belongs to User
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+module.exports = { User, Post, Comment };
