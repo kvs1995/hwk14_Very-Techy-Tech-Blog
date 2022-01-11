5fadb1622bf6db403f2da50a37a606234d99c836
Yelena Belova <42819374+kvs1995@users.noreply.github.com>
2022-01-11 03:08:01 -0600

    Post.js model build

--- models/Post.js
+++ models/Post.js
@@ -4,8 +4,40 @@ const sequelize = require('../config/connection');
 
 
 //extend mode
+class Post extends Model {}
 
 //Post init
-//id, title, contents, user_id, date_created
+Post.init(
+    //id, title, contents, user_id, date_created
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false, 
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        content: {
+            type: DataTypes.INTEGER,
+            allowNull: false, 
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key:'id',
+            },
+        },
+    }    
+);
 
 //module export
+module.exports = Post;
