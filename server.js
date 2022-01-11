c31aaa1a6e4c6ced9c9e14734e8ef41b829a43b2
Yelena Belova <42819374+kvs1995@users.noreply.github.com>
2022-01-11 03:36:08 -0600

    server.js pseudocode

---
+++ server.js
@@ -0,0 +1,14 @@
+//import modules: path, express, session, exphbs, routes, helpers, sequelize, SequelizeStore, app, port
+
+
+//set up handlebars.js engine with custom helpers. 
+
+//create sess variables
+
+//middleware: use-sess
+
+//middleware: engine('handlebars', hbs.engine);
+
+//middleware: ap.use express.json(), urlencoded, static(path.join(__dirnaem, 'public', routes))
+
+//sequelize sync to inform the server is listening 
