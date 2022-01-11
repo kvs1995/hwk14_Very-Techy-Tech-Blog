3a474dced329f65438bc86e57761acee1f03e6b0
Yelena Belova <42819374+kvs1995@users.noreply.github.com>
2022-01-11 02:48:49 -0600

    User.js model build

--- models/User.js
+++ models/User.js
@@ -4,7 +4,34 @@ const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
 //extend Model
+class User extends Model{}
+
 //id, username, password
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false, 
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true, 
+        modelName: 'user',
+    }
+);
 
 //module export
-
+module.exports = Project;
